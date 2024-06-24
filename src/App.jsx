6dcb5410e1896085d5fa8079620d50b0e17665bb
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2024-06-24 11:20:12 +0630

    Frame setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [name, setName] = useState("")
+  const [surName, setSurName] = useState("")
+  console.log(surName);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className=' w-screen h-screen flex flex-col justify-center items-center'>
+      {!surName && (
+        <form onSubmit={(e) => { e.preventDefault(); setSurName(name) }}>
+          <input
+            type="text"
+            placeholder="Your Name"
+            className="input input-bordered input-accent w-full max-w-xs" onChange={(e) => setName(e.target.value)} />
+        </form>
+      )}
+      {surName && (
+        <>
+          <p>Hello {surName}, welcome to my Secret Page.</p>
+          <div className=' mt-10'>
+            <p>Do You Wanna Go Next Slide ??</p>
+            <div className=" flex justify-center items-center">
+              <button>Yes</button>
+              <button>No</button>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   )
 }
 
