e06bf38b195a91449a4f282a41b7c8baa902c965
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2024-06-25 12:59:07 +0630

    Slide Images added

--- src/paths/Path.jsx
+++ src/paths/Path.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Main from '../pages/Main'
 import DragSlide from '../pages/DragSlide'
+import TextPage from '../pages/TextPage'
 
 const Path = () => {
     return (
@@ -9,6 +10,7 @@ const Path = () => {
             <Routes>
                 <Route path='/' element={<Main />} />
                 <Route path='drag' element={<DragSlide />} />
+                <Route path='/textPage' element={<TextPage />} />
             </Routes>
         </div>
     )
